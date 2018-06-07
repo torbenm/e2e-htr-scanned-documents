a2245bc3b8caca1d326125ef5d7e129b7583c5b6
Torben Meyer <torben.meyer@mailbox.org>
2018-06-07 21:34:29 +0200

    stick with channels last for now

---
+++ README.md
@@ -0,0 +1,1 @@
+nohup python train.py --config reshtr-iam-lines --gpu 0 > logs/full-18-06-07.txt 2>&1 &
--- nn/reshtr.py
+++ nn/reshtr.py
@@ -33,7 +33,7 @@ class ResHtr(AlgorithmBase):
 
         ResNet = resnet.Model(resnet_size=3, bottleneck=False, num_filters=16, kernel_size=3,
                               conv_stride=1, first_pool_size=2, first_pool_stride=2,
-                              block_sizes=[3] * 3, block_strides=[1] * 3)
+                              block_sizes=[3] * 3, block_strides=[1] * 3, data_format='channels_last')
 
         net = wrap_1d(ResNet(x, is_train))
 
