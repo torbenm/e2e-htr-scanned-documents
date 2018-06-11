0306bd2e7ff137448783e94225d5b44a9c685374
Torben Meyer <torben.meyer@mailbox.org>
2018-06-11 20:58:15 +0200

    updated train.sh

--- train.sh
+++ train.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 echo $1
-
-export WORKON_HOME=~/Envs
-source /usr/local/bin/virtualenvwrapper.sh
-workon env1
-
-nohup python train.py --config $1 --gpu 0 > logs/$1-$(date +%F-%T).txt 2>&1 &
+file=logs/$1-$(date +%F-%T).txt
+echo $file
+nohup python train.py --config $1 --gpu 0 > $file  2>&1 &
+tail -f $file -n 100
