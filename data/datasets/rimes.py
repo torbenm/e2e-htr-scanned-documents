a7c09d5d006cf092be6392c7b202341b9d5ea695
Torben Meyer <torben.meyer@mailbox.org>
2018-06-16 15:39:38 +0200

    finalize rimes dataset

--- data/datasets/rimes.py
+++ data/datasets/rimes.py
@@ -41,50 +41,47 @@ class RimesDataset(Dataset):
         return self.identifier
 
     def _load_ascii_lines(self, basepath, type, limit=-1):
-        with open(os.path.join(basepath, self.identifier, "ascii/{}.txt".format(type)), "r") as lines:
-            parsed = []
-            fulltext = ""
-            i = 0
-            while limit == -1 or i < limit:
-                line = lines.readline().strip()
-                if not line:
-                    break
-                if line[0] != "#":
-                    lsplit = line.split(" ")
-                    i = i + 1
-                    if lsplit[1] != "err":
-                        path = self._get_image_path(basepath, type, lsplit[0])
-                        text = ' '.join(lsplit[8:])
-                        parsed.append({"path": path, "truth": text})
-                        fulltext = fulltext + text
-            return parsed, fulltext
-
-    def _get_image_path(self, basepath, subset, identifier):
-        idsplit = identifier.split("-")
-        return os.path.join(basepath, self.identifier, subset, idsplit[0], "-".join(idsplit[0:2]), identifier + ".png")
+        parsed = util.loadJson(basepath, "images")
+        if limit > -1:
+            parsed = parsed[:limit]
+        fulltext = ""
+        for line in parsed:
+            fulltext += line["truth"]
+        return parsed, fulltext
 
     def do_split(self, path):
         split_path = os.path.join(path, "split")
         img_split_path = os.path.join(split_path, "imgs")
-        # TODO: remove
-        util.rmkdir(img_split_path)
-        # if not os.path.exists(split_path):
-        # os.makedirs(img_split_path)
-        tree = xml.dom.minidom.parse(
-            os.path.join(path, TRUTH_FILE)).documentElement
-        limit = 5
-        for page in tree.getElementsByTagName("SinglePage"):
-            for paragraph in page.getElementsByTagName("Paragraph"):
-                image_name = os.path.basename(
-                    paragraph.getAttribute("FileName"))
-                img_basename = '.'.join(image_name.split('.')[:-1])
-                image_path = os.path.join(path, IMAGE_PATH, image_name)
-                img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-
-                for line in paragraph.getElementsByTagName("Line")
-                    crop_img = img
-                    cv2.imshow("cropped", crop_img)
-                    cv2.waitKey(0)
-                    limit -= 1
-                    if limit == 0:
-                        exit()
+        if not os.path.exists(split_path):
+            os.makedirs(img_split_path)
+            tree = xml.dom.minidom.parse(
+                os.path.join(path, TRUTH_FILE)).documentElement
+            limit = 100
+            images = []
+            for page in tree.getElementsByTagName("SinglePage"):
+                for paragraph in page.getElementsByTagName("Paragraph"):
+                    image_name = os.path.basename(
+                        page.getAttribute("FileName"))
+                    img_basename = '.'.join(image_name.split('.')[:-1])
+                    img_ext = image_name.split('.')[-1]
+                    image_path = os.path.join(path, IMAGE_PATH, image_name)
+                    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+                    i = 0
+                    if img is not None:
+                        for line in paragraph.getElementsByTagName("Line"):
+                            output_path = os.path.join(
+                                img_split_path, '{}-{}.{}'.format(img_basename, str(i), img_ext))
+                            x_l = max(int(line.getAttribute("Left")), 0)
+                            x_r = int(line.getAttribute("Right"))
+                            y_b = int(line.getAttribute("Bottom"))
+                            y_t = max(int(line.getAttribute("Top")), 0)
+                            truth = line.getAttribute("Value")
+                            crop_img = img[y_t:y_b, x_l:x_r]
+                            images.append({
+                                "truth": truth,
+                                "path": output_path
+                            })
+                            cv2.imwrite(output_path, crop_img)
+                            i += 1
+            print(str(len(images))+" Images extracted")
+            util.dumpJson(split_path, "images", images)
