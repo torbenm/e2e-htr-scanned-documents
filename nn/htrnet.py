259851f50e794d1c0628f8e09cae8ea3426d1109
Torben Meyer <torben.meyer@mailbox.org>
2018-06-23 15:41:35 +0200

    fix htrnet

--- nn/htrnet.py
+++ nn/htrnet.py
@@ -85,6 +85,8 @@ class HtrNet(AlgorithmBaseV2):
         if self['format'] == 'nchw':
             net = log_1d(tf.transpose(net, [0, 2, 3, 1]))
 
+        net = wrap_1d(tf.transpose(net, [0, 2, 1, 3]))
+
         ################
         # PHASE II: Recurrent Block
         ###############
