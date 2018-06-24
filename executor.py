fe864c0323e6d9a290dc53865a2ff440e190e19d
Torben Meyer <torben.meyer@mailbox.org>
2018-06-24 22:50:18 +0200

    fix tensorboard integration

--- executor.py
+++ executor.py
@@ -12,7 +12,7 @@ MODELS_PATH = "./models"
 CONFIG_PATH = "./config"
 TENSORBOARD_PATH = "./tensorboard"
 
-global_step = 0
+global_step = 1
 
 
 class Executor(object):
@@ -112,7 +112,6 @@ class Executor(object):
             run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
             run_metadata = tf.RunMetadata()
         # Batch loop
-        global_step = 0
         for X, Y, length in epoch:
             if hooks is not None and 'batch' in hooks:
                 hooks['batch'](idx, steps, batch_num)
@@ -124,7 +123,7 @@ class Executor(object):
                 graph['is_train']: True
             }
             training_loss_ = [0]
-            if global_step % 100 == 0:
+            if global_step % 50 == 0:
                 training_loss_, _, s = sess.run(
                     [graph['total_loss'], graph['train_step'], summ], train_dict,
                     run_metadata=run_metadata, options=run_options)
