514d336969cd0af87ac7efe42ad4cf8b3de8c240
Torben Meyer <torben.meyer@mailbox.org>
2018-08-12 19:14:34 +0200

    forward args

--- train.sh
+++ train.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 echo $1
 file=logs/$1-$(date +%F-%T).txt
 echo $file
-nohup python train.py --config $1 --gpu 3 > $file  2>&1 &
+nohup python train.py --config $1 --gpu 3 ${@:2} > $file  2>&1 &
 tail -f $file -n 100
