f42986dad5729bb45aa8b0b0aee0b3c4f7605176
Torben Meyer <torben.meyer@mailbox.org>
2018-09-09 17:22:39 +0200

    readd final output

--- nn/layer/tf_unet/util.py
+++ nn/layer/tf_unet/util.py
@@ -109,8 +109,8 @@ def combine_img_prediction(data, gt, pred):
     img = np.concatenate((to_rgb(crop_to_shape(data, pred.shape).reshape(-1, ny, ch)),
                           to_rgb(crop_to_shape(
                               gt[..., 1], pred.shape).reshape(-1, ny, 1)),
-                          #   to_rgb(np.float32(np.argmax(pred, 3)
-                          #                     ).reshape(-1, ny, 1), False),
+                          to_rgb(np.float32(np.argmax(pred, 3)
+                                              ).reshape(-1, ny, 1), False),
                           #   to_rgb(pred[..., 0].reshape(-1, ny, 1)),
                           to_rgb(pred[..., 1].reshape(-1, ny, 1))), axis=1)
     return img
