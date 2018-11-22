fix recall precision for separator

lib/executables/Extendable.py
@@ -67,32 +67,32 @@ class Extendable(object):
         if self._tp is None:
             pred_res = self.build_pred_res(graph)
             y_res = self.build_y_res(graph)
-            self._tp = tf.reduce_mean(
-                tf.cast(tf.equal(tf.boolean_mask(pred_res, tf.equal(y_res, 0)), 0), tf.float32))
+            self._tp = tf.reduce_sum(tf.cast(tf.equal(tf.boolean_mask(
+                pred_res, tf.equal(y_res, 0)), 0), tf.float32))
         return self._tp
 
     def build_fp(self, graph):
         if self._fp is None:
             pred_res = self.build_pred_res(graph)
             y_res = self.build_y_res(graph)
-            self._fp = tf.reduce_mean(
-                tf.cast(tf.equal(tf.boolean_mask(pred_res, tf.equal(y_res, 1)), 0), tf.float32))
+            self._fp = tf.reduce_sum(tf.cast(tf.equal(tf.boolean_mask(
+                pred_res, tf.equal(y_res, 1)), 0), tf.float32))
         return self._fp
 
     def build_fn(self, graph):
         if self._fn is None:
             pred_res = self.build_pred_res(graph)
             y_res = self.build_y_res(graph)
-            self._fn = tf.reduce_mean(
-                tf.cast(tf.equal(tf.boolean_mask(pred_res, tf.equal(y_res, 0)), 1), tf.float32))
+            self._fn = tf.reduce_sum(tf.cast(tf.equal(tf.boolean_mask(
+                pred_res, tf.equal(y_res, 0)), 1), tf.float32))
         return self._fn
 
     def build_tn(self, graph):
         if self._tn is None:
             pred_res = self.build_pred_res(graph)
             y_res = self.build_y_res(graph)
-            self._tn = tf.reduce_mean(
-                tf.cast(tf.equal(tf.boolean_mask(pred_res, tf.equal(y_res, 1)), 1), tf.float32))
+            self._tn = tf.cast(tf.equal(tf.boolean_mask(
+                pred_res, tf.equal(y_res, 1)), 1), tf.float32)
         return self._tn
 
     def build_sep_accuracy(self, graph):
@@ -125,20 +125,17 @@ class Extendable(object):
             self._sep_prec = tp / (tp+fp)
         return self._sep_prec
 
-    
     def build_sep_fmeasure(self, graph):
         if self._sep_f is None:
             prec = self.build_sep_precision(graph)
             rec = self.build_sep_recall(graph)
-            self._sep_f = (tf.constant(2.0) * prec * rec)/ (prec+rec)
+            self._sep_f = (tf.constant(2.0) * prec * rec) / (prec+rec)
         return self._sep_f
 
     def build_pred_res(self, graph):
         if self._pred_res is None:
             self._pred_res = tf.argmax(graph['output'], 3)
         return self._pred_res
-    
-
 
     def build_y_res(self, graph):
         if self._y_res is None:

lib/executables/SeparationValidator.py
@@ -25,9 +25,12 @@ class SeparationValidator(Executable, Extendable):
         }
 
     def extend_graph(self, graph):
-        self.build_sep_precision(graph)
-        self.build_sep_recall(graph)
-        self.build_sep_fmeasure(graph)
+        # self.build_sep_precision(graph)
+        # self.build_sep_recall(graph)
+        # self.build_sep_fmeasure(graph)
+        self.build_tp(graph)
+        self.build_fn(graph)
+        self.build_tn(graph)
 
     def max_batches(self):
         return self.config.defaultchain(
@@ -38,30 +41,41 @@ class SeparationValidator(Executable, Extendable):
             self.config['batch'], max_batches=self.max_batches(), dataset=self.subset)
 
     def get_graph_executables(self, graph):
-        return [graph['loss'], self._sep_f, self._sep_rec, self._sep_prec]
+        return [graph['loss'], self._tn, self._fn, self._tp]
 
     def before_call(self):
         self.all_training_loss = []
-        self.f_total = []
-        self.prec_total = []
-        self.rec_total = []
+        self.tp = 0
+        self.fn = 0
+        self.tn = 0
+        # self.f_total = []
+        # self.prec_total = []
+        # self.rec_total = []
 
     def after_iteration(self, batch, execution_results):
-        training_loss, f, rec, prec = execution_results
+        training_loss, tn, fn, tp = execution_results
+        # training_loss, f, rec, prec = execution_results
         self.all_training_loss.append(training_loss)
-        self.f_total.append(f)
-        self.rec_total.append(rec)
-        self.prec_total.append(prec)
+        self.tp += tp
+        self.fn += fn
+        self.tn += tn
+        # self.f_total.append(f)
+        # self.rec_total.append(rec)
+        # self.prec_total.append(prec)
 
     def after_call(self):
         self.training_loss = np.ma.masked_invalid(
             self.all_training_loss).mean()
-        self.mean_f = np.ma.masked_invalid(
-            self.f_total).mean()
-        self.mean_rec = np.ma.masked_invalid(
-            self.rec_total).mean()
-        self.mean_prec = np.ma.masked_invalid(
-            self.prec_total).mean()
+        # self.mean_f = np.ma.masked_invalid(
+        #     self.f_total).mean()
+        # self.mean_rec = np.ma.masked_invalid(
+        #     self.rec_total).mean()
+        # self.mean_prec = np.ma.masked_invalid(
+        #     self.prec_total).mean()
+        self.mean_rec = self.tp / (self.tp+self.fn)
+        self.mean_prec = self.tp / (self.tp+self.fp)
+        self.mean_f = 2.0 * (self.mean_prec * self.mean_rec) / \
+            (self.mean_prec + self.mean_rec)
 
     def summarize(self, summary):
         summary.update({