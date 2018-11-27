ab8015b2b2fbca29ebf536c87746d8f1e50dc251
Torben Meyer <torben.meyer@mailbox.org>
2018-11-27 14:52:58 +0100

    fix viz

--- lib/buildingblocks/visualizer/SeparatedVisualizer.py
+++ lib/buildingblocks/visualizer/SeparatedVisualizer.py
@@ -12,7 +12,7 @@ class SeparatedVisualizer(object):
     def __init__(self, config={}):
         self.config = Configuration(config, DEFAULT_CONFIG)
 
-    def __call__(self, original, merged):
+    def __call__(self, original, merged, is_gt=False):
         if len(original.shape) > 2 and original.shape[2] == 3:
             original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
         return np.concatenate((original, merged), axis=1)
